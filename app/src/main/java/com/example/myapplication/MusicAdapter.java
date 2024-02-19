ad815bac506825b1bab71dde75dcd1a2ef6ea589
Hatomoyo2721 <zackchickien@gmail.com>
2024-02-19 23:38:33 +0700

    Thêm nút menu cho mỗi đối tượng 'nhạc', function delete nhạc

--- app/src/main/java/com/example/myapplication/MusicAdapter.java
+++ app/src/main/java/com/example/myapplication/MusicAdapter.java
@@ -1,22 +1,29 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaMetadata;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -61,12 +68,54 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.viewHolder> 
                     music_Context.startActivity(i);
                 }
             });
+
+            //Menu trên item để xóa nếu muốn
+            holder.menuMore.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PopupMenu popupMenu = new PopupMenu(music_Context, v);
+                    popupMenu.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                    popupMenu.show();
+                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.delete:
+                                    Toast.makeText(music_Context, "Đang xóa...", Toast.LENGTH_SHORT).show();
+                                    deleteFile(position, v);
+                                    break;
+                            }
+                            return true;
+                        }
+                    });
+                }
+
+            });
         } catch (IOException e) {
             e.printStackTrace();
             // Handle the exception, e.g., load a default image or log the error
             Glide.with(music_Context)
                     .load(R.drawable.question_mark)
                     .into(holder.album_Art);
+        }
+    }
+
+    //Xóa file nhạc
+    private void deleteFile(final int position, View v) {
+        Uri contentUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                Long.parseLong(music_Files.get(position).getId())); //Context//
+
+        File file = new File(music_Files.get(position).getPath());
+        boolean deleted = file.delete();
+        if (deleted) {
+            music_Context.getContentResolver().delete(contentUri, null, null);
+            music_Files.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, music_Files.size());
+            Snackbar.make(v, "Đã xóa", Snackbar.LENGTH_SHORT).show();
+        }
+        else {
+            Snackbar.make(v, "Không thể xóa", Snackbar.LENGTH_SHORT).show();
         }
     }
 
@@ -78,11 +127,12 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.viewHolder> 
     //Lưu trữ các thành phần UI (User interface) cho mỗi mục trong list: file + ảnh album
     public class viewHolder extends RecyclerView.ViewHolder {
         TextView file_Name;
-        ImageView album_Art;
+        ImageView album_Art, menuMore;
         public viewHolder(@NonNull View itemView) {
             super(itemView);
             file_Name = itemView.findViewById(R.id.music_file_name);
             album_Art = itemView.findViewById(R.id.music_img);
+            menuMore = itemView.findViewById(R.id.menuMore);
         }
     }
 
