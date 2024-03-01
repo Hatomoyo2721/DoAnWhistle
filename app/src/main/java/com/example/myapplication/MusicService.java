5e592f95628695fdd06e8577538d992e12e228bd
Hatomoyo2721 <zackchickien@gmail.com>
2024-03-01 16:21:28 +0700

    add functions to use instead of old functions in PlayerActivity.java

--- app/src/main/java/com/example/myapplication/MusicService.java
+++ app/src/main/java/com/example/myapplication/MusicService.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.PlayerActivity.listSongs;
+
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -14,11 +17,18 @@ import java.security.Provider;
 import java.util.ArrayList;
 
 //27 - 02 - 2024
-public class MusicService extends Service {
+public class MusicService extends Service implements MediaPlayer.OnCompletionListener {
     //IBinder là giao thức kết nối giữa Server và 1 thành phần khác (thường là activity)
     IBinder myBinder = new MyBinder();
     MediaPlayer mediaPlayer;
     ArrayList<MusicFiles> musicFiles = new ArrayList<>();
+    Uri uri;
+    int position = -1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 
     @Nullable
     @Override
@@ -26,7 +36,6 @@ public class MusicService extends Service {
         Log.e("Bind", "Method");
         return myBinder;
     }
-
 
     //1 activity connect to services
     public class MyBinder extends Binder {
@@ -37,6 +46,73 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        int myPosition = intent.getIntExtra("servicePosition", -1);
+        if (myPosition != -1) {
+            playMedia(myPosition);
+        }
+        return START_STICKY;
+    }
+
+    private void playMedia(int StartPosition) {
+        musicFiles = listSongs;
+        position = StartPosition;
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            if (musicFiles != null) {
+                createMediaPlayer(position);
+                mediaPlayer.start();
+            }
+        }
+        else {
+            createMediaPlayer(position);
+            mediaPlayer.start();
+        }
+    }
+
+    void start() {
+        mediaPlayer.start();
+    }
+
+    boolean isPlaying() {
+        return  mediaPlayer.isPlaying();
+    }
+
+    void stop() {
+        mediaPlayer.stop();
+    }
+
+    void release() {
+        mediaPlayer.release();
+    }
+
+    void pause() {
+        mediaPlayer.pause();
+    }
+
+    int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    void seekTo(int position) {
+        mediaPlayer.seekTo(position);
+    }
+
+    int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    void createMediaPlayer(int position) {
+        uri = Uri.parse(musicFiles.get(position).getPath());
+        mediaPlayer = MediaPlayer.create(getBaseContext(), uri);
+    }
+
+    void OnCompleted() {
+        mediaPlayer.setOnCompletionListener(this);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
     }
 }
