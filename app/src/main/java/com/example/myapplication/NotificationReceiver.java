8d0726fb842aaac70c023b8a54a6d1935b4c4c73
Hatomoyo2721 <zackchickien@gmail.com>
2024-03-03 18:20:03 +0700

    start service

--- app/src/main/java/com/example/myapplication/NotificationReceiver.java
+++ app/src/main/java/com/example/myapplication/NotificationReceiver.java
@@ -1,5 +1,9 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.ApplicationClass.ACTION_NEXT;
+import static com.example.myapplication.ApplicationClass.ACTION_PLAY;
+import static com.example.myapplication.ApplicationClass.ACTION_PREVIOUS;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +11,29 @@ import android.content.Intent;
 public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        String actionName = intent.getAction();
+        Intent serviceIntent = new Intent(context, MusicService.class);
+        if (actionName != null) {
+            switch (actionName) {
+                case ACTION_PLAY:
+//                    serviceIntent.putExtra("ActionName", intent.getAction());
+                    serviceIntent.putExtra("ActionName", "playPause");
+                    context.startService(serviceIntent);
+                    break;
 
+                case ACTION_NEXT:
+//                    serviceIntent.putExtra("ActionName", intent.getAction());
+                    serviceIntent.putExtra("ActionName", "Next");
+
+                    context.startService(serviceIntent);
+                    break;
+
+                case ACTION_PREVIOUS:
+//                    serviceIntent.putExtra("ActionName", intent.getAction());
+                    serviceIntent.putExtra("ActionName", "Previous");
+                    context.startService(serviceIntent);
+                    break;
+            }
+        }
     }
 }
