58800b5728585fe3fd6b19c10c2d847f5052d657
Hatomoyo2721 <zackchickien@gmail.com>
2024-03-08 23:29:13 +0700

    test failed...

--- app/src/main/java/com/example/myapplication/DatabaseActivity.java
+++ app/src/main/java/com/example/myapplication/DatabaseActivity.java
@@ -1,53 +1,34 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldPath;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.SetOptions;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.List;
 
 public class DatabaseActivity extends AppCompatActivity {
-    private static final String TAG = "DatabaseActivity";
-
-    public static final String KEY_TITLE = "title";
-    public static final String KEY_DESCRIPTION = "description";
 
     private EditText editTextTitle, editTextDescription, editTextPriority;
     private TextView textViewData;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference notebookRef = db.collection("Notebook");
-    private DocumentReference noteRef = db.document("Notebook/My Test Note");
 
+    private DocumentSnapshot lastResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,40 +39,6 @@ public class DatabaseActivity extends AppCompatActivity {
         editTextDescription = findViewById(R.id.edit_text_description);
         editTextPriority = findViewById(R.id.edit_text_priority);
         textViewData = findViewById(R.id.text_view_data);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
-                if (e != null) {
-                    return;
-                }
-
-                String data = "";
-
-                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                    Note note = documentSnapshot.toObject(Note.class);
-                    note.setDocumentId(documentSnapshot.getId());
-
-                    String documentId = note.getDocumentId();
-                    String title = note.getTitle();
-                    String description = note.getDescription();
-                    int priority = note.getPriority();
-
-                    data += "ID: " + documentId +
-                            "\nTitle: " + title +
-                            "\nDescription: " + description +
-                            "\nPriority: " + priority +
-                            "\n\n";
-
-                }
-
-                textViewData.setText(data);
-            }
-        });
     }
 
     public void addNote(View v) {
@@ -110,38 +57,23 @@ public class DatabaseActivity extends AppCompatActivity {
 
     }
 
-//    public void updateDescription(View v) {
-//        String description = editTextDescription.getText().toString();
-//
-//        //Map<String, Object> note = new HashMap<>();
-//        //note.put(KEY_DESCRIPTION,description);
-//
-//        //noteRef.set(note, SetOptions.merge());
-//        noteRef.update(KEY_DESCRIPTION, description);
-//    }
-//
-//    public void deleteDescription(View v) {
-//        //Map<String, Object> note = new HashMap<>();
-//        //note.put(KEY_DESCRIPTION, FieldValue.delete());
-//
-//        //noteRef.update(note);
-//        noteRef.update(KEY_DESCRIPTION, FieldValue.delete());
-//    }
-//
-//    public void deleteNote(View v) {
-////        noteRef.update(KEY_TITLE, FieldValue.delete());
-//        noteRef.delete();
-//    }
-
     public void loadNotes(View v) {
-       notebookRef.whereGreaterThanOrEqualTo("priority", 1)
-               .orderBy("priority", Query.Direction.DESCENDING)
-               .limit(3)
-               .get()
-               .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                   @Override
-                   public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                       String data = "";
+        Query query;
+        if (lastResult == null) {
+            query = notebookRef.orderBy("priorty")
+                    .limit(3);
+        } else {
+            query = notebookRef.orderBy("priorty")
+                    .startAfter(lastResult)
+                    .limit(3);
+        }
+
+        query.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
                         for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
                             Note note = documentSnapshot.toObject(Note.class);
                             note.setDocumentId(documentSnapshot.getId());
@@ -151,14 +83,21 @@ public class DatabaseActivity extends AppCompatActivity {
                             String description = note.getDescription();
                             int priority = note.getPriority();
 
-                            data += "ID: " + documentId +
-                                    "\nTitle: " + title +
-                                    "\nDescription: " + description +
-                                    "\nPriority: " + priority +
-                                    "\n\n";
+                            data += "ID: " + documentId
+                                    + "\nTitle: " + title
+                                    + "\nDescription: " + description
+                                    + "\nPriority: " + priority
+                                    + "\n\n";
                         }
-                        textViewData.setText(data);
-                   }
-               });
+
+                        if (queryDocumentSnapshots.size() > 0) {
+                            data += "_________\n\n";
+                            textViewData.append(data);
+
+                            lastResult = queryDocumentSnapshots.getDocuments()
+                                    .get(queryDocumentSnapshots.size() - 1);
+                        }
+                    }
+                });
     }
 }
